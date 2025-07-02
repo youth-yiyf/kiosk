db9e15f1a89e16dd04e7a404b53fcff00181b906
youth-yiyf <sujiyouth@naver.com>
2025-07-02 14:23:16 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase 웹 앱 초기화 정보 (Firebase 콘솔에서 복사한 값으로 바꾸세요)
+const firebaseConfig = {
+  apiKey: "AIzaSyBnW7copyPKtw-t_I3nCCyK5h5yZyUKtcA",
+  authDomain: "youthreservationapp.firebaseapp.com",
+  databaseURL: "https://youthreservationapp-default-rtdb.firebaseio.com",
+  projectId: "youthreservationapp",
+  storageBucket: "youthreservationapp.firebasestorage.app",
+  messagingSenderId: "205168190902",
+  appId: "1:205168190902:web:0156a544807a9479870d17",
+  measurementId: "G-18GLC0NZX7"
+};
+
+// Firebase 초기화
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database(); // 실시간 데이터베이스 인스턴스
